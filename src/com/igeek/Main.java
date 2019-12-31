824b65c149eb83ce690de9353e054ae9109226d2
fengxuanhai <1094741987@qq.com>
2019-12-31 15:49:56 +0800

    add hello git01 by fxh in second commit

--- src/com/igeek/Main.java
+++ src/com/igeek/Main.java
@@ -9,6 +9,6 @@ package com.igeek;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git01");
     }
 }
