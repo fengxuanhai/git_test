715335cccf687d272f0790d5d2c6c2875f516b7e
fengxuanhai <1094741987@qq.com>
2019-12-31 15:40:12 +0800

    first commit

---
+++ src/com/igeek/Main.java
@@ -0,0 +1,14 @@
+package com.igeek;
+
+/**
+ * @description:
+ * @author: 封宣海
+ * @date: Created in 2019/12/31 15:35
+ * @version:
+ * @modified By:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
